cb740f6d6ea97d5b2587c669a456cad73dd7eb0d
MelanieHernandez99 <hernandezmelanie826@gmail.com>
2026-06-29 17:48:18 -0300

    eliminando archivos duplicados

--- Creacion de DB/VIEWS.sql
+++ Creacion de DB/VIEWS.sql
@@ -43,3 +43,18 @@ GROUP BY da.idDetalle, a.Nombre, m.Nombre, r.Descripcion, t.Nombre, c.Nombre
 HAVING ISNULL(SUM(s.Cantidad),0) <= 5;
 
 
+CREATE VIEW Vista_ArticulosMasVendidos AS
+SELECT 
+    A.Nombre AS NombreArticulo,
+    M.Nombre AS Marca,
+    R.Nombre AS Categoria,
+    SUM(DV.Cantidad) AS CantidadTotalVendida,
+    SUM(DV.Cantidad * DV.PrecioUnitario) AS RecaudacionTotal
+FROM Articulo A
+JOIN DetalleArticulo DA ON A.idArticulo = DA.idArticulo
+JOIN DetalleVenta DV ON DA.idDetalle = DV.idDetalle
+JOIN Venta V ON DV.IdVenta = V.IdVenta
+JOIN Marca M ON A.IdMarca = M.IdMarca
+JOIN Rubro R ON A.IdRubro = R.IdRubro
+GROUP BY A.Nombre, M.Nombre, R.Nombre;
+GO
